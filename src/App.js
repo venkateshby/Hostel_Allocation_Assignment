db434b25958d2da70aeaf90a76e4d1e4810c3e2c
venkateshby <venkateshby1998@gmail.com>
2021-08-20 18:13:19 +0530

    hostel-allocation-firstPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from "react"
 import './App.css';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user);
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hostel Allocation</h1>
+          <p>Please Sign-in</p>
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <h3>You have already booked a room</h3>
+            <h2>Your room details as follows:</h2>
+            <h4>Hostel No. <span class="red">B4</span></h4>
+            <h4>Room No. <span class="red">8</span></h4>
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import firebase from "firebase/app";
+import * as firebase from "firebase/app";
 
-// Use your config values here.
-firebase.initializeApp({
+var firebaseConfig = {
     apiKey: "AIzaSyBz_EBdzM3sqbbwqWBeWArgxQ7smt9reTM",
     authDomain: "hostel-allocation-assign-b0013.firebaseapp.com",
     projectId: "hostel-allocation-assign-b0013",
@@ -12,12 +8,9 @@ firebase.initializeApp({
     messagingSenderId: "671491148112",
     appId: "1:671491148112:web:e338fe9ac8d6890ffac455",
     measurementId: "G-EPPSYYD9PC"
-});
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+  export default firebase;
